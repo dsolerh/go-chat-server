2189e0564085ca9f0c58196888195435ee57c685
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-22 03:31:40 +0200

    add a func to create new rooms

--- cmd/room.go
+++ cmd/room.go
@@ -24,6 +24,15 @@ type room struct {
 	clients map[*client]bool
 }
 
+func NewRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
 func (r *room) run() {
 	for {
 		select {
