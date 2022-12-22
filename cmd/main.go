211e133ccdd19d51ae86ef6ee2620afb0db001a3
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-22 03:31:54 +0200

    use the room as a handler

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,13 @@ import (
 )
 
 func main() {
+	r := NewRoom()
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
 
 	// start the web server
 	log.Println("Starting server on port: 8080")
