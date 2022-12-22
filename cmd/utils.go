dbc763d6ffc008a153b4410976edf7ccd7dbe477
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-22 02:05:11 +0200

    add a struct for serving templates

---
+++ cmd/utils.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
