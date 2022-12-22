134f12e8bb75fdb8eaa351a2927aae4b2ede8891
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-22 02:05:12 +0200

    update the server to use the template helper

--- cmd/main.go
+++ cmd/main.go
@@ -6,18 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-        <html>
-           <head>
-             <title>Chat</title>
-           </head>
-           <body>
-             Let's chat!
-           </body>
-        </html>
-        `))
-	})
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the web server
 	log.Fatal("ListenAndServe:", http.ListenAndServe(":8080", nil))
