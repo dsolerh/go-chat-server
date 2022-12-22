bff1522b6bd07ad1168f49641cbcb86d21201658
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-22 02:13:18 +0200

    add log on start the server

--- cmd/main.go
+++ cmd/main.go
@@ -9,5 +9,6 @@ func main() {
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// start the web server
+	log.Println("Starting server on port: 8080")
 	log.Fatal("ListenAndServe:", http.ListenAndServe(":8080", nil))
 }
