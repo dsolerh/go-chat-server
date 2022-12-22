4fb1fc6edf160afd7873643a246f5774fb55f88f
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-22 17:08:15 +0200

    make the address to the server dynamic

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application")
+	flag.Parse()
+
 	r := NewRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -15,6 +19,6 @@ func main() {
 	go r.run()
 
 	// start the web server
-	log.Println("Starting server on port: 8080")
-	log.Fatal("ListenAndServe:", http.ListenAndServe(":8080", nil))
+	log.Println("Starting server on address:", *addr)
+	log.Fatal("ListenAndServe:", http.ListenAndServe(*addr, nil))
 }
