95c13c28667441d9e44437e0817125ca8d66dfca
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-28 15:28:47 +0200

    configure the credential for gomniauth

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,24 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application")
 	flag.Parse()
+
+	// setup gomniauth
+	gomniauth.SetSecurityKey("SECRET KEY")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("825102912466-bcqv49ighbhpdkeqgmb5v1a6buhcgjlv.apps.googleusercontent.com", "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", "http://localhost:8080/auth/callback/google"),
+	)
 
 	r := NewRoom()
 
