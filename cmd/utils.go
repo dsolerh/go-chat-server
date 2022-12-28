391ec711ed1eeb9fed74c436118a287fb0f99e2b
dsolerh <dsolerh.cinter95@gmail.com>
2022-12-28 16:23:19 +0200

    update the template info

--- cmd/utils.go
+++ cmd/utils.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/stretchr/objx"
 )
 
 // templ represents a single template
@@ -18,5 +20,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+
+	t.templ.Execute(w, data)
 }
