7e7fdd2d18e805add51b75d1af766a1f2c1dbc15
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-04 17:17:34 +0200

    update the client and room to use the new message interface

--- cmd/client.go
+++ cmd/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -11,19 +12,24 @@ type client struct {
 	// socket is the web socket for this client.
 	socket *websocket.Conn
 	// send is a channel on which messages are sent.
-	send chan []byte
+	send chan *message
 	// room is the room this client is chating in.
 	room *room
+	// the user information
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
 	defer c.socket.Close()
 	for {
-		_, msg, err := c.socket.ReadMessage()
+		var msg *message
+		err := c.socket.ReadJSON(msg)
 		if err != nil {
 			log.Println("Error reading:", err)
 			return
 		}
+		msg.Timestamp = time.Now()
+		msg.Name = c.userData["name"].(string)
 		c.room.forward <- msg
 	}
 }
@@ -31,7 +37,7 @@ func (c *client) read() {
 func (c *client) write() {
 	defer c.socket.Close()
 	for msg := range c.send {
-		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		err := c.socket.WriteJSON(msg)
 		if err != nil {
 			log.Println("Error writing:", err)
 			return
--- cmd/room.go
+++ cmd/room.go
@@ -17,7 +17,7 @@ const (
 type room struct {
 	// forward is a channel that holds incoming messages
 	// that should be forwarded to the other clients.
-	forward chan []byte
+	forward chan *message
 	// join is a channel for clients wishing to join the room.
 	join chan *client
 	// join is a channel for clients wishing to leave the room.
@@ -31,7 +31,7 @@ type room struct {
 
 func NewRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -52,7 +52,7 @@ func (r *room) run() {
 			close(client.send)
 			r.tracer.Trace("Client left")
 		case msg := <-r.forward:
-			r.tracer.Trace("Message received:", string(msg))
+			r.tracer.Trace("Message received:", (msg))
 			// forward message to all clients
 			for client := range r.clients {
 				client.send <- msg
@@ -78,7 +78,7 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 	client := &client{
 		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
+		send:   make(chan *message, messageBufferSize),
 		room:   r,
 	}
 	r.join <- client
