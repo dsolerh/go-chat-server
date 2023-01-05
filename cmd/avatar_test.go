543ea09c8e177617ac44fe763fccde504d8f6703
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-05 14:43:44 +0200

    test the avatar implementation

---
+++ cmd/avatar_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/objx"
+)
+
+func TestAuthAvatar(t *testing.T) {
+	var authAvatar AuthAvatar
+	client := new(client)
+
+	url, err := authAvatar.GetAvatarURL(client)
+	if err != ErrNoAvatarURL {
+		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
+	}
+
+	// set a value
+	testURL := "http://url-to-gravatar/"
+	client.userData = objx.Map{"avatar_url": testURL}
+
+	url, err = authAvatar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("AuthAvatar.GetAvatarURL should return no error when value present")
+	}
+
+	if url != testURL {
+		t.Error("AuthAvatar.GetAvatarURL should return the correct URL")
+	}
+}
