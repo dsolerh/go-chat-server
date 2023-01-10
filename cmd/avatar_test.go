67c5a062e0b8b2f8d70d5c50ce4291503d43f16c
dsolerh <dsolerh.cinter95@gmail.com>
2023-01-10 17:12:06 +0200

    update the testing of the interface

--- cmd/avatar_test.go
+++ cmd/avatar_test.go
@@ -6,23 +6,29 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/stretchr/objx"
+	test "github.com/stretchr/gomniauth/test"
 )
 
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
-	client := new(client)
 
-	url, err := authAvatar.GetAvatarURL(client)
+	testUser := &test.TestUser{}
+	testUser.On("AvatarURL").Return("", ErrNoAvatarURL)
+
+	testChatUser := &chatUser{User: testUser}
+
+	url, err := authAvatar.GetAvatarURL(testChatUser)
 	if err != ErrNoAvatarURL {
 		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
 	}
 
 	// set a value
 	testURL := "http://url-to-gravatar/"
-	client.userData = objx.Map{"avatar_url": testURL}
+	testUser = &test.TestUser{}
+	testChatUser.User = testUser
+	testUser.On("AvatarURL").Return(testURL, nil)
 
-	url, err = authAvatar.GetAvatarURL(client)
+	url, err = authAvatar.GetAvatarURL(testChatUser)
 	if err != nil {
 		t.Error("AuthAvatar.GetAvatarURL should return no error when value present")
 	}
@@ -34,28 +40,27 @@ func TestAuthAvatar(t *testing.T) {
 
 func TestGravatarAvatar(t *testing.T) {
 	var gravatarAvatar GravatarAvatar
-	client := new(client)
-	client.userData = objx.Map{
-		"userid": "55502f40dc8b7c769880b10874abc9d0",
-	}
-	url, err := gravatarAvatar.GetAvatarURL(client)
+	user := &chatUser{uniqueID: "abc"}
+
+	url, err := gravatarAvatar.GetAvatarURL(user)
 	if err != nil {
 		t.Error("GravatarAvatar.GetAvatarURL should not return an error")
 	}
-	if url != "//www.gravatar.com/avatar/55502f40dc8b7c769880b10874abc9d0" {
+
+	if url != GRAVATAR_URL+"abc" {
 		t.Errorf("GravatarAvatar.GetAvatarURL wrongly returned %s", url)
 	}
 }
 
 func TestFileSystemAvatar(t *testing.T) {
+	// make a test avatar file
 	filename := filepath.Join("avatars", "abc.jpg")
 	ioutil.WriteFile(filename, []byte{}, 0777)
 	defer os.Remove(filename)
 
 	var fileSystemAvatar FileSystemAvatar
-	client := new(client)
-	client.userData = objx.Map{"userid": "abc"}
-	url, err := fileSystemAvatar.GetAvatarURL(client)
+	user := &chatUser{uniqueID: "abc"}
+	url, err := fileSystemAvatar.GetAvatarURL(user)
 	if err != nil {
 		t.Error("FileSystemAvatar.GetAvatarURL should not return an error")
 	}
